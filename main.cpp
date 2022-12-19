8411f8d865d72e3b528461934784f80957d66e3b
Rea1HumanBean <88336698+Rea1HumanBean@users.noreply.github.com>
2022-12-20 00:32:42 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,8 +7,9 @@ private:
 	std::string c_name;
 	unsigned int c_price = 0;
 	unsigned int c_lvl = 0;
+	
 public:
-	details( std::string name, unsigned int price, unsigned int lvl)
+	details(std::string name, unsigned int price, unsigned int lvl)
 	{
 		c_name = name;
 		c_price = price;
@@ -38,15 +39,16 @@ private:
 	unsigned int c_price = 0;
 	unsigned int c_lvl = 0;
 	std::vector<details*> x_details;
+
 public:
 	car(std::string name, unsigned int price, unsigned int lvl)
 	{
 		c_name = name;
 		c_price = price;
-		 
+
 	}
 
-	void AddDetails(details* detail){
+	void AddDetails(details* detail) {
 		x_details.push_back(detail);
 	}
 
@@ -54,20 +56,26 @@ public:
 		std::cout << "Car info" << std::endl;
 		std::cout << " You got a " << c_name << std::endl;
 		for (int i = 0; i < x_details.size(); i++)
-			std::cout << x_details[i]->Name()<<" Level details - "<< x_details[i]->Level() << std::endl;
+			std::cout << x_details[i]->Name() << " Level details - " << x_details[i]->Level() << std::endl;
 	}
 	int Price()
 	{
 		return c_price;
 	}
+
+	void remove() {
+		x_details.clear();
+	}
+
+	~car();
 };
 
 
 int main()
 {
 	int n = 10000, n1 = n, m_x;
-	
-	details* engine_1 = new details("stock engine", 1500, 1);
+
+	details* engine_1 = new details("stock engine", 1500, 1 );
 	details* transmission_1 = new details("stock transmission", 1500, 1);
 	details* body_1 = new details("stock body", 0, 1);
 	details* wheels_1 = new details("stock wheels", 0, 1);
@@ -78,16 +86,16 @@ int main()
 	if (m_x == 1) {
 		car* car_1 = new car("Bucket", 2500, 1);
 		car_1->AddDetails(engine_1);
-		car_1->AddDetails(transmission_1); 
+		car_1->AddDetails(transmission_1);
 		car_1->AddDetails(body_1);
 		car_1->AddDetails(wheels_1);
 		car_1->infoCar();
 		n = n - car_1->Price() - engine_1->Price_D() - transmission_1->Price_D();
-		std::cout << "Money = "<< n<<std::endl;
+		std::cout << "Money = " << n << std::endl;
+		car_1->remove();
 	}
 	else {
 		std::cout << "You don't have cars" << std::endl;
 		return 0;
 	}
-	delete car;
 }
